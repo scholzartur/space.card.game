96b011a5f8c7a9aa324fa3e6a34e1a8101dbb5db
Artur Scholz <arturscholz@outlook.com>
2020-02-09 23:13:24 +0100

    [frontend] add drag and drop

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,34 +4,56 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'space-card-game';
 
-  todo = [
+  allStarships = [
     'Get to work',
     'Pick up groceries',
     'Go home',
     'Fall asleep'
   ];
 
-  done = [
-    'Get up',
-    'Brush teeth',
-    'Take a shower',
-    'Check e-mail',
+  fightingSideOne = [
     'Walk dog'
   ];
+
+  fightingSideTwo = [
+    'Get up',
+  ];
+
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
+      this.swapElements(event);
       transferArrayItem(event.previousContainer.data,
                         event.container.data,
                         event.previousIndex,
                         event.currentIndex);
     }
   }
+
+  swapElements(event: CdkDragDrop<string[]>) {
+    switch (event.container.id) {
+      case 'fightingSideTwo': {
+        this.swap(this.fightingSideTwo);
+        break;
+      }
+      case 'fightingSideOne': {
+        this.swap(this.fightingSideOne);
+        break;
+      }
+    }
+  }
+
+  private swap(array: string[]) {
+    this.allStarships.push(array[0]);
+    delete array[0];
+    array.length = 0;
+  }
+
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoMaterialModule } from './material-module';
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 import '../polyfills';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DemoMaterialModule} from './material-module';
-import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
-
-
 
 @NgModule({
   declarations: [
@@ -21,13 +19,13 @@ import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     DemoMaterialModule,
     MatNativeDateModule,
     ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
